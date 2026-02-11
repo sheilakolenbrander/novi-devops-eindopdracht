c4fe9794de99b92b609bcdee0df932758263274e
Sheila Kolenbrander <Sheila.Kolenbrander@etesian.nl>
2026-02-11 20:44:57 +0100

    Stricter CORS policy

--- novi-devops-eindopdracht.Server/Program.cs
+++ novi-devops-eindopdracht.Server/Program.cs
@@ -25,7 +25,6 @@ builder.Services.AddCors(options =>
                     .WithOrigins(allowedOrigins)
                     .AllowAnyHeader()
                     .AllowAnyMethod()
-                    .AllowCredentials();
             }
         });
 });
